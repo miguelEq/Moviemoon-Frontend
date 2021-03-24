10224414e7858aa0c87abda15fb47575d23d4d7b
miguel angel <miguelequilea18@gmail.com>
2021-03-23 21:07:51 -0300

    refactor navbarhome

--- src/components/navbarHome.js
+++ src/components/navbarHome.js
@@ -4,32 +4,40 @@ import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import { withRouter } from 'react-router';
-
+import pikachu from '../images/pikachu.jpg'
 class NavBarHome extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             input: "",
-            resultSearch:[]
+            resultSearch: []
         }
     }
-    setInput = (event) => {  }
+    setInput = (event) => { }
 
     executeSearch = () => {
     }
     render() {
-        return (<Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Moviemoon</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-          <Nav>
-          <Form inline >
-          <Form.Control type="text" placeholder="Search" className="mr-sm-2" size="lg" />
-          <Button variant="outline-primary">Search</Button>
-          </Form>
-          </Nav> 
-        </Navbar.Collapse>
-      </Navbar>)
+        return (
+            <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+                <img
+                    src={pikachu}
+                    width="80"
+                    height="70"
+                    className="d-inline-block align-top"
+                    alt="Pikachu Moviemoon logo"
+                />
+                <Navbar.Brand > <strong>Moviemoon</strong></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                    <Nav>
+                        <Form inline >
+                            <Form.Control type="text" placeholder="Search" className="mr-sm-3" size="md" />
+                            <Button variant="outline-primary">Search</Button>
+                        </Form>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>)
     }
 }
 export default withRouter(NavBarHome)
