d663f50d6a42a775c5a719040420db6b5d8f8726
miguel angel <miguelequilea18@gmail.com>
2021-03-23 23:25:38 -0300

    add navbarNavigate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter,Switch,Route} from 'react-router-dom';
 import NavBarHome from './components/navbarHome';
+import NavBarNavigate from './components/navbarNavigate';
 
 export default class App extends React.Component{
   constructor(props){
@@ -12,8 +13,9 @@ render(){
      <BrowserRouter>
       <Switch>
           <Route exact path="/" render={props => <NavBarHome></NavBarHome> } ></Route>
+          <Route exact path="/player-video" render={props=><NavBarNavigate></NavBarNavigate>}></Route>
       </Switch>
-       </BrowserRouter>
+     </BrowserRouter>
        );
 }
 }
---
+++ src/components/navbarNavigate.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
+import pikachu from '../images/pikachu.jpg'
+class NavBarNavigate extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: "",
+            resultSearch: []
+        }
+    }
+    setInput = (event) => { }
+
+    executeSearch = () => {
+    }
+    render() {
+        return (
+            <Navbar collapseOnSelect  bg="dark" variant="dark">
+                <Nav className="container-fluid">
+                <img
+                    src={pikachu}
+                    width="80"
+                    height="70"
+                    className="d-inline-block align-top"
+                    alt="Pikachu Moviemoon logo"
+                />
+                <Navbar.Brand><strong>Moviemoon</strong></Navbar.Brand>
+                <Nav.Item className="ml-auto">
+                <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+                </Nav.Item>
+                </Nav>
+            </Navbar>)
+    }
+}
+export default withRouter(NavBarNavigate)
